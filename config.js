0d322e2947e704b233ec8794fb2150d2b0b1d6d9
Netanel77 <netanelshmueli11@gmail.com>
2026-08-04 17:11:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 // (see README.md for exact steps).
 //
 // Settings → API → Project URL
-export const SUPABASE_URL = "https://akhmthistvnfkpwytwjz.supabase.co/rest/v1/";
+export const SUPABASE_URL = "https://akhmthistvnfkpwytwjz.supabase.co";
 //
 // Settings → API → Project API keys → anon / public
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFraG10aGlzdHZuZmtwd3l0d2p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxNjc2OTYsImV4cCI6MjEwMDc0MzY5Nn0.zBNP9rTHc3bk6lnQNTy5I-8xgn0sL7OLzEznfV7_fQ0";
