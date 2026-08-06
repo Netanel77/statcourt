687abbb3d5ab5c406b6ddfc5bd087c3699d836bd
Netanel77 <netanelshmueli11@gmail.com>
2026-08-06 18:50:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+console.log("HELLO STATCOURT");
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from "./config.js";
 import { translations, t } from "./i18n.js";
